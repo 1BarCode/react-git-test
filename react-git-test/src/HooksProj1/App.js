167e4bc09b24ab445c6f7eb07533aa9bca0072f7
1BarCode <69029687+1BarCode@users.noreply.github.com>
2021-03-24 00:25:05 -0700

    Set up react hook project
    rendered list and added key property
    -need to remove the html element from fetched results

--- react-git-test/src/HooksProj1/App.js
+++ react-git-test/src/HooksProj1/App.js
@@ -1,14 +1,11 @@
-import './App.css';
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import Search from "./components/Search";
 
 class App extends Component {
   render() {
-
-  
     return (
       <div className="App">
-        
+        <Search />
       </div>
     );
   }
